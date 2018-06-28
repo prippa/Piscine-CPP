0886c3a73d4f740ae5351bf38df0e58405f21208
prippa <pavelrippa@gmail.com>
2018-06-28 20:28:52 +0300

    d08 ex00

---
+++ d08/ex00/easyfind.hpp
@@ -0,0 +1,27 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <iterator>
+# include <vector>
+# include <list>
+
+// template <typename T>
+// unsigned int	easyfind(T & type, int element)
+// {
+// 	T::iterator it_begin = type.begin();
+// 	T::iterator it_end = type.end();
+// 	unsigned int i = 0;
+
+// 	while (it_begin != it_end)
+// 	{
+// 		if (*it_begin == element)
+// 			return (i);
+// 		it_begin++;
+// 		i++;
+// 	}
+// 	throw std::range_error("Element not found!");
+// 	return (-1);
+// }
+
+#endif
---
+++ d08/ex00/main.cpp
@@ -0,0 +1,8 @@
+#include "easyfind.hpp"
+
+int		main(void)
+{
+	std::lsit<int> my_list(1,2,3);
+	// std::list<int> first;
+	return (0);
+}
